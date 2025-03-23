a4eb66a83321d2c13514ce3d71fe76d6997ed04e
nour <nour3lwan99@gmail.com>
2025-03-24 01:29:04 +0300

    tasks java

--- javascriptassignmenr/script.js
+++ javascriptassignmenr/script.js
@@ -46,3 +46,67 @@ console.log(myname.concat(lname));
 let st13= " javascript world world";
 console.log(st13.indexOf("pt"));
 console.log(st13.lastIndexOf("ld"));
+
+
+//task
+let arr12 = [1, 2, 3, 4, 5, 6];
+console.log(arr12.map((x) => x * 3));
+
+let arr13 = [1, 2, 2, 3, 3, 4, 5]; //[1,2,3,4,5] x,y,z => value ,index , array
+let res3 = arr13.filter((x, y, z) => z.indexOf(x) === y);
+console.log(res3);
+
+let arr0 = [
+  { name: "hussam", age: 30 },
+  { name: "ali", age: 40 },
+  { name: "ahmad", age: 22 },
+];
+arr0.sort((a, b) => a.age - b.age); // ترتب الارري من الاقل للاكبر
+console.log(arr0);
+
+
+let arr6= [1,5,9,6,3,87,72,23]
+
+function largeNum(arr6){
+    return Math.max(...arr6)
+}
+
+console.log(largeNum(arr6))
+
+
+
+function reverseArray(arr) {
+    const reversedArray = [];
+    for (let i = arr.length - 1; i >= 0; i--) {
+        reversedArray.push(arr[i]); 
+    }
+    return reversedArray;
+}
+
+const myArray = [1, 2, 3, 4, 5];
+const reversed = reverseArray(myArray);
+
+console.log(reversed);
+
+
+
+let array= [1,5,9,6,3,87,72,23]
+
+function sumNum(array,target){
+array.sort((a,b => a-b));
+
+let left =0;
+let right = array.length-1;
+
+while(left< right){
+    let sum =array[left] + array[right];
+    if (sum === target){
+        return [array[left], array[right]];
+    } else if (sum< target){
+        left ++;
+    } else{
+        right --;
+    }
+}
+return null;
+}
